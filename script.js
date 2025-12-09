52412f449fa033a1cbdc7fe15c48682904f8f97e
Aurelie Raout <aurelieraout@gmail.com>
2025-12-09 14:15:26 +0100

    first step of the app is functionnal. Speech to text base feature is ok

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
+const recordBtn = document.getElementById('recordBtn');
+const resultBox = document.getElementById('resultBox');
+
+let isRecording = false;
+let speechObj = null;
+
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
-recognition.lang = 'fr-FR';
+if (!SpeechRecognition) {        // Browser not supported
+  recordBtn.innerText = "Speech Recognition Not Supported in Firefox. Try with Chrome please";
+  recordBtn.disabled = true;
+}
+
+
+recordBtn.addEventListener('click', () => {
+  isRecording = !isRecording;
+  isRecording ? startRecording() : stopRecording();
+});
+
+function startRecording() {
+  recordBtn.innerText = "J'écoute...";
+  speechObj = new SpeechRecognition(); // nouvelle instance à chaque démarrage
+  speechObj.start(); // démarrer la reconnaissance vocale
+  speechObj.onresult = transcribe; // gérer les résultats
+  speechObj.onerror = (event) => {
+  console.error("Erreur de reconnaissance vocale :", event.error);
+  recordBtn.innerText = "Erreur";
+  isRecording = false;
+};
+};
+
+function transcribe(event) {
+  const { transcript } = event.results[0][0];
+  resultBox.textContent += `${transcript} `; // afficher le texte transcrit
+}
+
+function stopRecording() {
+  speechObj.stop(); // arrêter la reconnaissance vocale
+  speechObj = null; // réinitialiser l'objet
+  recordBtn.innerText = "Start";
+  isRecording = false;
+}
